f2d5813fdc45e54a40a5aaa494a1c86b8a439c5d
Nolankicks <nolandimensions@gmail.com>
2024-10-31 20:33:46 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -22,7 +22,7 @@ const config = {
         }
     },
 
-    extensions: ['.svelte', '.md', '.svx'],
+    extensions: ['.svelte', '.svx'],
     preprocess: [
         vitePreprocess(),
         autoImport({
